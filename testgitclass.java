d513d8998b832b19a2a493171354390965dd103f
Youssef2350 <youssefbs2350@gmail.com>
2023-10-20 18:30:04 +0100

    classe java commit

---
+++ testgitclass.java
@@ -0,0 +1,14 @@
+public Class Personne {
+private String nom ;
+
+private String prenom ;
+
+private int age ;
+
+void Personne(String nom , String prenom , int n) {
+this.nom=nom ;
+this.prenom = prenom ;
+this.age = age ;
+
+}
+}
