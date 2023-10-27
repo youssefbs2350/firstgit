4a4c4b5510ab6a04af8ae5f0bc5b16c041dad263
Youssef2350 <youssefbs2350@gmail.com>
2023-10-27 18:27:33 +0100

    modifié par youssef2350

--- testgitclass.java
+++ testgitclass.java
@@ -5,6 +5,19 @@ private String prenom ;
 
 private int age ;
 
+public int getn {
+return (n) ;
+}
+
+public String getnNom{
+    return (nom) ;
+    }
+
+    public String getnPrenom{
+        return (prenomnom) ;
+        }
+    
+
 void Personne(String nom , String prenom , int n) {
 this.nom=nom ;
 this.prenom = prenom ;
